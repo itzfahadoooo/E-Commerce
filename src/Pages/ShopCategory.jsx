6db1aa298d5f43126e8a6dfdccf604adafe8f35f
itzfahadoooo <foyindolapo@gmail.com>
2024-12-06 11:14:08 +0100

    Initial commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -8,12 +8,12 @@ const ShopCategory = (props) => {
   const {all_product}=useContext(ShopContext)
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className="shopcategory-banner" src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
         </p>
-        <div>
+        <div className="shopcategory-sort">
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
@@ -30,6 +30,9 @@ const ShopCategory = (props) => {
             return null;
           }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
     </div>
   );
