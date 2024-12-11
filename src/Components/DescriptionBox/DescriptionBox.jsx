773886d94c767620ef3767001bd8df27df440527
itzfahadoooo <foyindolapo@gmail.com>
2024-12-11 23:53:09 +0100

    Initial commit

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import "./DescriptionBox.css"
+import React from "react";
+import "./DescriptionBox.css";
 
 const DescriptionBox = () => {
   return (
-    <div className='descriptionbox'>
-        <div className='descriptionbox-navigator'>
-            <div className="decriptionbox-nav-box">Description</div>
-            <div className="decriptionbox-nav-box fade ">Reviews (122)</div>
-            <div className="descriptionbox-description">
-                <p>An e-commerce website is an online platform that facilitates buying and selling of products or services over the internet serves as a virtual marketplace where businesses and in showcase their products, interact with customers, and create transactions without the need for a physical presence. websites have gained immense popularity due to their accessibility, and the global reach they offer</p>
-                    <p>E-commerce websites typically display products or services detailed descriptions, images, prices, and any available (e.g., sizes, colors). Each product usually has its own with relevant information.</p>
-            </div>
-
-
-        </div>
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="decriptionbox-nav-box">Description</div>
+        <div className="decriptionbox-nav-box fade ">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          An e-commerce website is an online platform that facilitates buying
+          and selling of products or services over the internet serves as a
+          virtual marketplace where businesses and in showcase their products,
+          interact with customers, and create transactions without the need for
+          a physical presence. websites have gained immense popularity due to
+          their accessibility, and the global reach they offer
+        </p>
+        <p>
+          E-commerce websites typically display products or services detailed
+          descriptions, images, prices, and any available (e.g., sizes, colors).
+          Each product usually has its own with relevant information.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DescriptionBox
+export default DescriptionBox;
